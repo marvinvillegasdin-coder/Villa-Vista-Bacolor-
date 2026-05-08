7d53e6e67fbeedbe330862daafc479524fe8e3de
marvinvillegasdin-coder <marvinvillegasdin@gmail.com>
2026-05-08 15:23:07 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Serve the chatbot
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Proxy route for Anthropic API
